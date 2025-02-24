29ca09c2f05f26ebafb24d86d2d7b8071ec3309d
AkmalFairuz <akmalf475@gmail.com>
2025-02-24 18:50:51 +0700

    Add KCP transport

---
+++ example/transport/kcp.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log/slog"
+
+	"github.com/cooldogedev/spectrum-df"
+	"github.com/cooldogedev/spectrum-df/transport"
+	"github.com/df-mc/dragonfly/server"
+	"github.com/df-mc/dragonfly/server/player/chat"
+)
+
+func main() {
+	log := slog.Default()
+	chat.Global.Subscribe(chat.StdoutSubscriber{})
+	conf, err := server.DefaultConfig().Config(log)
+	if err != nil {
+		panic(err)
+	}
+
+	conf.Listeners = []func(conf server.Config) (server.Listener, error){func(conf server.Config) (server.Listener, error) {
+		return spectrum.NewListener(":19133", nil, transport.NewKCP())
+	}}
+	srv := conf.New()
+	srv.CloseOnProgramEnd()
+	srv.Listen()
+	for range srv.Accept() {
+	}
+}
---
+++ transport/kcp.go
@@ -0,0 +1,40 @@
+package transport
+
+import (
+	"github.com/xtaci/kcp-go"
+	"io"
+	"net"
+)
+
+type KCP struct {
+	listener net.Listener
+}
+
+func NewKCP() *KCP {
+	return &KCP{}
+}
+
+// Listen ...
+func (k *KCP) Listen(addr string) (err error) {
+	listener, err := kcp.Listen(addr)
+	if err != nil {
+		return err
+	}
+	k.listener = listener
+	return
+}
+
+// Accept ...
+func (k *KCP) Accept() (io.ReadWriteCloser, error) {
+	c, err := k.listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// Close ...
+func (k *KCP) Close() error {
+	return k.listener.Close()
+}
